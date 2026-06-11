f2f7cbe39a1b136ef2f9213ad814b6d2cfae89ab
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2026-06-12 04:50:47 +0700

    style: center brand header and headings on mobile, automate gradle build sync

--- docs/assets/ManagementDashboard-CSNNBCyo.js
+++ docs/assets/ManagementDashboard-CSNNBCyo.js
@@ -1,4 +1,4 @@
-import{c as O,r as h,g as At,j as e,A as ce,U as Ke,C as Oe,S as Tt,T as Be,F as Ee,a as me,b as Fe,d as pe,e as He,f as Ge,M as _e,h as zt,i as It,k as Ue,l as Je,m as Ye,n as Ve}from"./index-CUH8SqUp.js";import{h as Nt}from"./haptics-D45ds25B.js";import{P as Ct}from"./phone-m3oA2Lbk.js";/**
+import{c as O,r as h,g as At,j as e,A as ce,U as Ke,C as Oe,S as Tt,T as Be,F as Ee,a as me,b as Fe,d as pe,e as He,f as Ge,M as _e,h as zt,i as It,k as Ue,l as Je,m as Ye,n as Ve}from"./index--YZxQVk-.js";import{h as Nt}from"./haptics-CfdsKjg1.js";import{P as Ct}from"./phone-C61s3SLi.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- docs/assets/phone-C61s3SLi.js
+++ docs/assets/phone-C61s3SLi.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CUH8SqUp.js";/**
+import{c as a}from"./index--YZxQVk-.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
