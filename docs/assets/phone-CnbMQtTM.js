0c80424b9d01a7dff673fb603a2317b36aeef416
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2026-06-12 05:23:33 +0700

    style: align mobile global-header symmetrically with content cards

--- docs/assets/phone-CnbMQtTM.js
+++ docs/assets/phone-CnbMQtTM.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DJtTWKGY.js";/**
+import{c as a}from"./index-BsuBcYdv.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
